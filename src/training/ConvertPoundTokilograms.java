dd1dbe6b0f645d0b7fa0851863ddd126b69ab0fd
Omar Qouqas (GN) <omar.qouqas@geant.org>
2019-10-01 16:59:46 +0100

    convert pounds to KGs

---
+++ src/training/ConvertPoundTokilograms.java
@@ -0,0 +1,15 @@
+package training.src.training;
+
+public class ConvertPoundTokilograms {
+
+	public static void main(String[] args) {
+
+		double poundsToKG = 200;
+		double valueInKGs = poundsToKG * 0.45359237;
+
+		System.out.println("poundsToKG to KG = " + poundsToKG);
+		System.out.println("Value in KGs = " + valueInKGs);
+
+	}
+
+}
